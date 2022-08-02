0d1af98dff63bc4207cfe6398f3f93561329272a
warcraftbroker <away.vas@gmail.com>
2022-08-02 17:47:49 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <HelloWorld msg="Welcom to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  background-color: #36393f;
+  margin: auto;
+  max-width: 1200px;
+  margin: auto;
+}
+a {
+  color: #00aff4;
+}
+</style>
