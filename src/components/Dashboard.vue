d5abbe93555854eaa47a22737f08cce1feba6ce6
warcraftbroker <away.vas@gmail.com>
2022-09-14 15:38:52 +0200

    Dashboard init

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,14 +1,7 @@
 <template>
-
 <div id="dashboard">
-  <br/>
-  <p>Hier befindet sich noch kein Inhalt</p>
-  <p>Wenn du helfen willst daran etwas zu ändern findest du dieses Projekt unter dem folgenden Link auf GitHub</p>
-  <p><svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github">
-    <path fill-rule="evenodd" fill="#DCDDDE" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
-</svg></p>
-<p><a href="http://www.github.com/warcraftbroker/website" target="_blank">http://www.github.com/warcraftbroker/website</a></p></div>
-
+  <apexchart width="500" type="line" :options="options" :series="series"></apexchart>
+</div>
 </template>
 
 <script>
@@ -16,6 +9,30 @@ export default {
   name: 'DashboardPage',
   props: {
     msg: String
+  },
+  data: function() {
+    return {
+      options: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [
+      "01 Jan",
+      "02 Jan",
+      "03 Jan",
+      "04 Jan",
+      "05 Jan",
+      "06 Jan"
+    ],
+    labels: {style: { colors: '#ffffff' }}
+        },
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 91]
+      }]
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import VueApexCharts from "vue3-apexcharts";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueApexCharts).mount('#app')
 
 import "bootstrap/dist/js/bootstrap.js"
