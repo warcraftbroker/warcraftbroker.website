4db0e73396211ce94bc05cc05ddd37c9ccdde850
warcraftbroker <away.vas@gmail.com>
2022-11-06 14:40:16 +0100

    update: github link location and content

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,16 @@
       </ul>
     </div>
     <router-view id="container"  />
+  
+  <div class="githublink">
+     <a href="http://www.github.com/warcraftbroker/website" target="_blank">1.0 alpha
+          <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github" data-v-3349ea18="">
+            <path fill-rule="evenodd" fill="#DCDDDE" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" data-v-3349ea18="">
+            </path>
+          </svg>
+    auf GitHub</a> 
   </div>
+</div>
 </template>
 
 <script>
@@ -84,7 +93,27 @@ h6{
 }
 p {
   clear:both;
+  padding: 0px 0.5rem 0px 0.5rem;
   padding:0px 0px 20px 0px;
   margin: 0px;
+}
+.githublink { 
+  width: 100%;
+  float:left;
+  padding: 0px 0.5rem 10px 0.5rem;
+  text-align: center;
+}
+.githublink > a:visited, .githublink > a:link{
+  color: #DCDDDE;
+  opacity: 0.3;
+  text-decoration: none;
+  padding: 0 4px 0 4px;
+}
+.githublink:hover {
+  opacity: 0.5;
+}
+.githublink svg{
+  margin: 0px 1px 1px 1px;
+  padding: 2px;
 }
 </style>
